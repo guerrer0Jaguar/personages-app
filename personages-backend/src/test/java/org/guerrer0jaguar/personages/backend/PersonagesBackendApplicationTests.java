f445cfeb4318bf9761839aea5f004f52fac74a1b
Julio C. Ramírez <juliocesar.rgz@gmail.com>
2025-11-06 17:52:20 -0600

    feat: Artefacto inicial del backend (microservicio con GET de ejemplo)

---
+++ personages-backend/src/test/java/org/guerrer0jaguar/personages/backend/PersonagesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.guerrer0jaguar.personages.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonagesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
