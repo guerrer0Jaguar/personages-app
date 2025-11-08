803f79c78f6e7f13594b34fa190c399eace0657f
Julio C. Ramírez <juliocesar.rgz@gmail.com>
2025-11-08 16:48:44 -0600

    chore: Se ignora folder 'tmp' en el manejo de control de version

--- personages-backend/src/main/java/org/guerrer0jaguar/personages/backend/Config.java
+++ personages-backend/src/main/java/org/guerrer0jaguar/personages/backend/Config.java
@@ -3,15 +3,22 @@ package org.guerrer0jaguar.personages.backend;
 import org.guerrer0jaguar.personages.backend.model.MediaType;
 import org.guerrer0jaguar.personages.backend.repository.MediaTypeRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
+@ConfigurationProperties(prefix = "personages-backend")
+@Getter
+@Setter
 public class Config {
 
+	private String pathToSaveImages = "tmp";
 	@Bean
 	CommandLineRunner initDatabase(MediaTypeRepository repository) {
 		return args -> {
--- personages-backend/src/main/java/org/guerrer0jaguar/personages/backend/repository/CustomImageRepositoryImpl.java
+++ personages-backend/src/main/java/org/guerrer0jaguar/personages/backend/repository/CustomImageRepositoryImpl.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Base64;
 
+import org.guerrer0jaguar.personages.backend.Config;
 import org.guerrer0jaguar.personages.backend.model.Image;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaContext;
@@ -17,10 +18,17 @@ import jakarta.persistence.EntityManager;
 public class CustomImageRepositoryImpl implements CustomImageRepository {
 
 	private final EntityManager em;
+	private final Path baseFolder;
 	
 	@Autowired
-	public CustomImageRepositoryImpl(JpaContext context) {
-		this.em = context.getEntityManagerByManagedType(Image.class);
+	public CustomImageRepositoryImpl(JpaContext context, Config config) {
+		this.em = context.getEntityManagerByManagedType(Image.class);		
+		this.baseFolder = Paths.get(config.getPathToSaveImages());
+		try {
+			Files.createDirectories(baseFolder);
+		} catch (IOException e) {
+			throw new RuntimeException("Error when trying to create directory to save images", e);
+		}
 	}
 
 	@Override
@@ -30,8 +38,9 @@ public class CustomImageRepositoryImpl implements CustomImageRepository {
 			return img;
 		}
 		
-		saveOnFileSystem(img);
+		saveOnFileSystem(img);		
 		em.persist(img);
+		img.setContent("");
 		return img;
 	}
 
@@ -40,13 +49,11 @@ public class CustomImageRepositoryImpl implements CustomImageRepository {
 		if(img.getName() == null || img.getContent() == null) {
 			return;
 		}
-		
-		Path filePath = Paths.get(img.getName());
-		
-		try {
-			
+				
+		try {			
 			String base64Image = img.getContent().split(",")[1];
 			byte[] imageBytes = Base64.getDecoder().decode(base64Image);
+			Path filePath = baseFolder.resolve(img.getName());
 			Files.write(filePath, imageBytes,StandardOpenOption.CREATE);
 			img.setUrl(filePath.toAbsolutePath().toString());
 		} catch (IOException e) {
